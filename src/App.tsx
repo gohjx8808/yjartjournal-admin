8683904ae53bd249b8b8dbd455d0c77c2caf122a
Jing Xuan Goh <jingxuan.goh@capbay.com>
2021-12-15 10:10:12 +0800

    integrated firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import {
   ChakraProvider, extendTheme,
 } from '@chakra-ui/react';
+import { initializeApp } from 'firebase/app';
 import * as React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { Route, Routes } from 'react-router';
-import Sample from './modules/auth/views/Login';
 import Router from './modules/Router';
 
 const App = () => {
@@ -20,6 +19,18 @@ const App = () => {
       },
     },
   });
+
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSENGER_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  };
+
+  initializeApp(firebaseConfig);
 
   const queryClient = new QueryClient();
 
